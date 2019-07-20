2844511311908d25cb97462db3ab9a461cb885a6
HeroBrine1st <herobrine1.videos@gmail.com>
2019-07-20 17:16:30 +0300

    Тестируем Payload API

--- Modules/TestModule.py
+++ Modules/TestModule.py
@@ -1,3 +1,5 @@
+import json
+
 from vk_api.bot_longpoll import VkBotEvent
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
@@ -17,7 +19,14 @@ class Module(ModuleManager.Module):
             def run(self, event: VkBotEvent, args, keys):
                 keyboard: VkKeyboard = VkKeyboard(one_time=True)
                 keyboard.add_button(label="Test", color=VkKeyboardColor.PRIMARY)
-                keyboard.add_button(label="Test2", color=VkKeyboardColor.DEFAULT, payload="1234")
+                keyboard.add_button(label="Test2", color=VkKeyboardColor.DEFAULT,
+                                    payload=client.create_payload(
+                                        self.module,
+                                        {
+                                            "test2"
+                                        }
+                                    )
+                                    )
                 client.get_api().messages.send(keyboard=keyboard.get_keyboard(),
                                                peer_id=event.obj.peer_id,
                                                random_id=get_random_id(),
@@ -26,3 +35,9 @@ class Module(ModuleManager.Module):
                 return True
 
         client.module_manager.add_command(KeyboardCommand(), self)
+
+    def on_payload(self, client: Fusion, event: VkBotEvent, payload):
+        client.get_api().messages.send(peer_id=event.obj.peer_id,
+                                       random_id=get_random_id(),
+                                       message="Получен payload: %s" % json.dumps(payload)
+                                       )
