6259777b85e45a0a6375253662183030082c1ffd
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2019-10-05 17:59:14 +0300

    Готовимся к полному рефакторингу

--- regextest.py
+++
@@ -1,9 +0,0 @@
-import re
-
-regex = re.compile(r"\[id(\d+)\|(.+?)\]")
-S = "/123 [id168958515|@herobrine1st_erq]"
-
-for _id in regex.findall(S):
-    print("id:", _id)
-
-print(regex.sub(r"<@\1>", S))
